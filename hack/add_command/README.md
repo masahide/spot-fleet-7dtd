75861802601cd2398744b81b233a83734c070a4c
YAMASAKI Masahide <masahide.y@gmail.com>
2024-07-14 10:58:32 +0900

    fix

--- hack/add_command/README.md
+++ hack/add_command/README.md
@@ -5,3 +5,10 @@
 
 https://discord.com/developers/docs/interactions/application-commands
 
+
+```bash
+python3 list_commands.py
+python3 del_command.py
+python3 add_command.py
+
+```
--- hack/add_command/del_command.py
+++ hack/add_command/del_command.py
@@ -5,13 +5,13 @@ import os
 
 appid = os.environ['DISCORD_APP_ID']
 token = os.environ['DISCORD_TOKEN']
+id = os.environ['COMMAND_ID']
 
 # url = "https://discord.com/api/v8/applications/<my_application_id>/commands"
-url = 'https://discord.com/api/v8/applications/{}/commands'.format(appid)
+url = 'https://discord.com/api/v8/applications/{}/commands/{}'.format(appid,id)
 headers = {"Authorization": "Bot {}".format(token)}
 
 r = requests.delete(url, headers=headers)
 print(r)
-print(json.dumps(json.loads(r.text), indent=2))
 
 
